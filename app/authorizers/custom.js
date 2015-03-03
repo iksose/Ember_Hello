c394b4a3a5dd73c5a005f2a38cdb130de628c259
iksose <iksose.pt3@gmail.com>
2015-03-03 15:18:11 -0600

    made navbar horizontal

--- app/authorizers/custom.js
+++ app/authorizers/custom.js
@@ -2,7 +2,7 @@ import Base from 'simple-auth/authorizers/base';
 
 export default Base.extend({
   authorize: function(jqXHR, requestOptions) {
-    console.log("Author")
+    console.log("Author");
     var promise = new Promise((resolve, reject) => {
       resolve({
         // access_token: "cranked",
